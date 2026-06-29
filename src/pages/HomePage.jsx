84d4f06251dcdc3c55b21097e486cba62118123b
Chinonye <faithchinonye53@gmailcom>
2026-06-29 18:35:24 +0200

    set up frontend pages, components,  and routing files

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,26 @@
+export function HomePage() {
+    return (
+        <>
+            <section>
+
+            </section>
+            <div>
+                <img alt="" />
+            </div>
+            <div>
+                <div>
+                    <h1>60+</h1>
+                    <p>Countries served</p>
+                </div>
+                <div>
+                    <h1>2000+</h1>
+                    <p>Verified Specialist</p>
+                </div>
+                <div>
+                    <h1>20,000+</h1>
+                    <p>Patients helped</p>
+                </div>
+            </div>
+        </>
+    )
+}
